97de949b9872b8cfe22db0b2ce99dbb5e8e36864
yun.luo <yun.luo@remarkdip.com>
2018-06-05 17:54:36 +0800

    user manage search box

--- src/components/system-setting/user-manage/Search.jsx
+++ src/components/system-setting/user-manage/Search.jsx
@@ -1,22 +1,59 @@
 import React from 'react'
-import { Form, Select, Input } from 'antd'
+import PropTypes from 'prop-types'
+import { Form, Select, Input, Button } from 'antd'
 
 const FormItem = Form.Item
 const Option = Select.Option
 
-const Search = () => {
-  return (
-    <Form layout='inline'>
-      <FormItem label="员工查询">
-        <Select style={{ width: 70 }}>
-          <Option value="86">+86</Option>
-        </Select>
-      </FormItem>
-      <FormItem>
-        <Input></Input>
-      </FormItem>
-    </Form>
-  )
+class Search extends React.PureComponent {
+  static propTypes = {
+    searchParams: PropTypes.shape({
+      name: PropTypes.string,
+      phone: PropTypes.string,
+      deptId: PropTypes.number,
+      roleId: PropTypes.number
+    })
+  }
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      userInputType: 'phone'
+    }
+  }
+  render () {
+    const { userInputType } = this.state
+    const { searchParams } = this.props
+
+    return (
+      <Form layout='inline'>
+        <FormItem label="员工查询">
+          <Select value={userInputType}>
+            <Option value="phone">员工电话</Option>
+            <Option value="name">员工姓名</Option>
+          </Select>
+        </FormItem>
+        <FormItem>
+          <Input value={searchParams[userInputType]}></Input>
+        </FormItem>
+        <FormItem label="部门">
+          <Select value={searchParams.deptId}>
+            <Option value="phone">员工电话</Option>
+            <Option value="name">员工姓名</Option>
+          </Select>
+        </FormItem>
+        <FormItem label="角色">
+          <Select value={searchParams.roleId}>
+            <Option value="phone">员工电话</Option>
+            <Option value="name">员工姓名</Option>
+          </Select>
+        </FormItem>
+        <FormItem>
+          <Button type="primary">搜索</Button>
+        </FormItem>
+      </Form>
+    )
+  }
 }
 
 export default Search
--- src/containers/system-setting/UserManage.jsx
+++ src/containers/system-setting/UserManage.jsx
@@ -4,12 +4,26 @@ import { Search } from '@components/system-setting/user-manage'
 import { PageHeader } from '@components/common'
 
 class UserManage extends React.PureComponent {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      searchParams: {
+        name: '', // 姓名
+        phone: '', // 电话
+        deptId: -1, // 部门id
+        roleId: -1 // 角色id
+      }
+    }
+  }
   render () {
+    const { searchParams } = this.state
+
     return (
       <div>
         <PageHeader title="人员管理"></PageHeader>
         <Card>
-          <Search></Search>
+          <Search searchParams={searchParams}></Search>
         </Card>
       </div>
     )
